cdd1e2ec8681401ed0e08e7a06803a9da0d9255a
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-10 12:38:22 +0400

    stage0Module3Task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float t = 19;
+        t /= 5;
+        System.out.println(t);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,19 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class TimeConvertor {
     public void convert(float minutes) {
+
+        float min,sec;
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter Minutes");
+
+        min=sc.nextFloat();
+        sec=min*60;
+
+        System.out.println("Seconds: "+sec);
+
     }
 }
