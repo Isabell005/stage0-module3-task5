9a99e57578f3085d7fefd128bf6b1d5063928932
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-10 14:30:24 +0400

    stage0Module3Task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         toBeDivided = 19;
-        float sum = toBeDivided % 5;
+        double sum = toBeDivided % 5;
         System.out.println(sum);
 
     }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class TimeConvertor {
     public void convert(float minutes) {
 
-        minutes = 5;
+        minutes = 11.5f;
         float second = minutes * 60;
         System.out.println(second);
 
