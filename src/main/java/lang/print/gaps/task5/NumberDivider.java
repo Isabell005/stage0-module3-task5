98548568796a7a1f46864e9d7d7dfed09cb6800c
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-10 14:43:03 +0400

    stage0Module3Task5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         toBeDivided = 19;
-        double sum = toBeDivided / 5;
+        double sum = toBeDivided / 5.0;
         System.out.println(sum);
 
     }
